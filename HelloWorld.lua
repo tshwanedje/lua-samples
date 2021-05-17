d401ca84740a3b33f85bd3825954802936a16ae2
davidjoffe <david@djoffe.com>
2021-05-17 22:00:36 +0200

    Add a simple "Hello World" beginner-level introductory sample for scripting for TLex (or tlTerm, or tlDatabase or tlCorpus etc.)

---
+++ HelloWorld.lua
@@ -0,0 +1,8 @@
+-- Simple "Hello World" messagebox sample
+-- To run this:
+-- 1. Open TLex/tlTerm/tlDatabase
+-- 2. Go to 'Tools' menu, select "Execute Lua Script", and select this file
+-- (This is the simplest way to run a 'global' Lua script in TLex, though there are many other ways - so e.g. to do Lua smart-styling you would not use this Tools menu, that is done elsewhere)
+
+tMessageBox("Hello World - Welcome to Lua scripting in TLex/tlTerm/tlDatabase", "Message");
+
